9c09ddc147eb4a2c039a78187e89e1f598849f81
Tristan Krass <tristankrass@gmail.com>
2020-02-21 09:29:26 +0200

    Add project files.

---
+++ Domain/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+
+
+        [MinLength()]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string BirthSurname { get; set; }
+    }
+}
